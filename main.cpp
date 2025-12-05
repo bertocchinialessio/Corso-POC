7fc75b19cbe5ef66814a3b41cf53120fcf773467
Alessio B <bertocchinialessio@gmail.com>
2025-12-05 17:12:11 +0100

    test per vedere se funziona il push

--- main.cpp
+++ main.cpp
@@ -2,11 +2,15 @@
 //Questo codice è puramente didattico
 //Potrebbe non funzionare
 //Aggiunta la funzione f2
+//Test per vedere se funziona il push
 int main(){
 	int a = 0;
 	cin >> a;
-	if(a>10)cout<<"maggiore"<<endl;
-	else cout<<"hello"<<endl;
+	if(a>10){
+		cout<<"maggiore"<<endl;
+	}else{
+		cout<<"hello"<<endl;
+	}
 	double f2(int a){
 		return a; 
 	}
