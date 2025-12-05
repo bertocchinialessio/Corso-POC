f35c8866fbe935c21fc4b3b84a057d6b9613aa82
Alessio B <alessio4IB@gmail.com>
2025-12-05 15:24:52 +0100

    Aggiunta funzione f2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 //programma per informatica
 //Questo codice è puramente didattico
 //Potrebbe non funzionare
+//Aggiunta la funzione f2
 int main(){
+	double f2(int a){
+		return a; 
+	}
 	cout << "Hello world!" << endl;
 }
