ba98d0c233b2375a233d90a623f83f414f78ffed
Alessio B <alessio4IB@gmail.com>
2025-12-05 16:38:42 +0100

    Commit riassuntivo modifica di a

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 //Potrebbe non funzionare
 //Aggiunta la funzione f2
 int main(){
+	int a = 0;
+	cin >> a;
+	if(a>10)cout<<"maggiore"<<endl;
+	else cout<<"hello"<<endl;
 	double f2(int a){
 		return a; 
 	}
